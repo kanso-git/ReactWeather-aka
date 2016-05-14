0cb0a8bf028a6d9e24974f1e923e8322397de460
Abdallah Kanso <abdallah.kanso@gmail.com>
2016-05-15 01:40:03 +0200

    preparation repo for heroku

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express  =  require('express');
+//define the prot for heroku
+const PORT = process.env.PORT || 3000;
+
+// set a middleware to redirect all the https requests to http
+app.use(function(req,res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+   //if it's already on http continue ...
+    next();
+  }else{
+    // redirect https req to  http req
+    res.redirect('http://'+req.hostname+req.url);
+  }
+});
+
 // Create the app
 var app = express();
 
-
+//set the static folder
 app.use(express.static('public'));
 
-app.listen(3000, function(){
- console.log('Express server is up on port 3000');
+// start the listener
+app.listen(PORT, function(){
+ console.log('Express server is up on port '+PORT);
 });
